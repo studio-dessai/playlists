3e327043b041d0a0e9ba298f79f2a4888e9d20d2
stragu <stephane.guillou@member.fsf.org>
2018-02-06 12:10:02 +1000

    correct date

--- scripts/didweplay.sh
+++ scripts/didweplay.sh
@@ -1,4 +1,3 @@
-# TODO: replace existing file
 # TODO: only create a file if term was found
 # TODO: create ./results" directory if it doesn't exist
 # TODO: output more stats in both the file and the terminal
@@ -6,22 +5,28 @@
 # replace space(s) for paths
 StrForName="${1// /_}" ;
 
+# create result file path to reuse
+ResultPath="./results/didweplay-$StrForName.md" ;
+
+# remove previous results; ignore non-existent files with -f flag
+rm -f "$ResultPath" ;
+
 # create a markdown file
-touch ./results/didweplay-"$StrForName".md ;
+touch "$ResultPath" ;
 
 # analyse
 for filename in ../20*.md ; do
   if cat $filename | grep -i "$1" ; then
     # -i flag is for "case insensitive"
     # append filename
-    echo $filename >> ./results/didweplay-"$StrForName".md ;
+    echo $filename >> "$ResultPath" ;
     # if search term found, append corresponding line
-    cat $filename | grep -i "$1" >> ./results/didweplay-"$StrForName".md ;
+    cat $filename | grep -i "$1" >> "$ResultPath" ;
   fi
 done
 
 # give feedback
-echo "Donskies! Results in ./results/didweplay-$StrForName.md" ;
+echo "Donskies! Results are listed in $ResultPath" ;
 echo "Number of occurrences:" ;
-# count number of lines as proxy for number of occurences
-cat ./results/didweplay-"$StrForName".md | grep -i "$1" | wc -l
+# count number of lines that contain the search term as proxy for number of occurences
+cat $ResultPath | grep -i "$1" | wc -l
